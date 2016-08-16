b479b9b1bfc19400dda6a54552cee23e78d8f8a7
zmcdaniel <zmcdaniel@gmail.com>
2016-08-15 17:59:15 -0700

    styling, layouts

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var ejsLayouts = require("express-ejs-layouts");
 
 app.use(require('morgan')('dev'));
+app.set("view engine", "ejs");
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('home');
 });
 
 var server = app.listen(process.env.PORT || 3000);
